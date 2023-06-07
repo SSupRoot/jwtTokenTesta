138dd18f2272088b634e98e7d41d0f00ca81c4e7
SSupRoot <thought3860@naver.com>
2023-06-08 02:09:58 +0900

    Initial commit

---
+++ src/main/java/com/example/jwtTokenTest/JwtTokenTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtTokenTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTokenTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtTokenTest/JwtTokenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtTokenTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
